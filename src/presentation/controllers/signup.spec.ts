d2a7f30e9f807bcbbfabc7d1c629068974585a09
Thalia Guedes <thalia.guedes17@gmail.com>
2024-11-15 00:41:51 -0300

    test: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -16,3 +16,20 @@ describe('SignUp Controller', () => {
 
     });
 });
+
+describe('SignUp Controller', () => {
+    test('should return 400 if no name is provided', () =>{
+        const sut = new SignUpController();
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
+            }
+        };
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new Error ('Missing param: email'));
+
+    });
+});
