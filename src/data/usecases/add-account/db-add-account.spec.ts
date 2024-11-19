61e7168074724086c6a5f646d2c86d67c37ca66b
Thalia Guedes <thalia.guedes17@gmail.com>
2024-11-19 16:49:20 -0300

    test: ensure DbAddAccount calls Encrypter with correct password

---
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -0,0 +1,22 @@
+import { DbAddAccount } from './db-add-account';
+
+describe('DbAddAccount Usecase', () => {
+    test('Should call Encrypter with correct password', async () => {
+        class EncrypterStub{
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            async encrypt(value: string): Promise<string> {
+                return new Promise(resolve => resolve('hashed_password'));
+            }
+        }
+        const encrypterStub = new EncrypterStub();
+        const sut = new DbAddAccount(encrypterStub);
+        const encryptSpy = jest.spyOn(encrypterStub, 'encrypt');
+        const accountData = {
+            name: 'valid_password',
+            email: 'valid_password',
+            password: 'valid_password' 
+        };
+        await sut.add(accountData);
+        expect(encryptSpy).toHaveBeenLastCalledWith('valid_password');
+    });
+});
