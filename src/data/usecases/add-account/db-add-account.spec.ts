6306c192c5a8d3eca0ccc1f55e1478a555f59295
Thalia Guedes <thalia.guedes17@gmail.com>
2024-11-21 10:46:55 -0300

    test: ensure DbAddAccount return an account on sucess

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -19,7 +19,7 @@ const makeAddAccountRepository = ():AddAccountRepository => {
             const fakeAccount = {
                 id: 'valid_id',
                 name: 'valid_name',
-                email: 'valid_name',
+                email: 'valid_email',
                 password: 'hashed_password'
             };
             return new Promise(resolve => resolve(fakeAccount));
@@ -98,5 +98,21 @@ describe('DbAddAccount Usecase', () => {
         };
         const promise = sut.add(accountData);
         await expect(promise).rejects.toThrow();
+    });
+    test('Should return an account on sucess', async () => {
+        const { sut } = makeSut();
+        const accountData = {
+            name: 'valid_name',
+            email: 'valid_email',
+            password: 'valid_password' 
+        };
+        const account = await sut.add(accountData);
+        expect(account).toEqual({
+            id: 'valid_id',
+            name: 'valid_name',
+            email: 'valid_email',
+            password: 'hashed_password' 
+
+        });
     });
 });
