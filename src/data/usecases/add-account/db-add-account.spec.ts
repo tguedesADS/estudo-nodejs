1280728cc2d5847c81b83ea24fbde39a778bfe35
Thalia Guedes <thalia.guedes17@gmail.com>
2024-11-21 10:09:43 -0300

    refactor: group all protocols in a single file

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,5 +1,4 @@
-import { resolve } from 'path';
-import { Encrypter } from '../../protocols/encrypter';
+import { Encrypter } from './db-add-account-protocols';
 import { DbAddAccount } from './db-add-account';
 
 interface SutTypes {
