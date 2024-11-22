523043a94ac96907e19f510d39e6766d22998851
Thalia Guedes <thalia.guedes17@gmail.com>
2024-11-22 10:50:24 -0300

    refactor: move mongodb methods to a helper

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,16 @@
+import { MongoClient } from 'mongodb';
+
+export const MongoHelper = {
+    client: null as MongoClient,
+
+    async connect (uri: string): Promise<void> {
+        this.client = await MongoClient.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopolofy: true
+        });
+    },
+
+    async disconnect() {
+        await this.client.close();
+    }
+};
